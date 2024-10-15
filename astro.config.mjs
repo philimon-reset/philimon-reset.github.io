15f5b3e0fbfe962d64035fcc26798fb82c7f663a
Philimon Ayalew <58902914+philimon-reset@users.noreply.github.com>
2024-10-15 15:41:09 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://philimon-reset.github.io",
-  base: "/philimon-reset.github.io",
   integrations: [
     mdx(),
     sitemap(),
