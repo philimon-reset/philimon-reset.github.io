c19f0fbaba030fa0266082d480a5c1726399f267
Philimon Derib Ayalew <philimonderib2019@gmail.com>
2024-10-15 15:02:02 +0200

    lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://philimon-reset.github.io",
+  base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
